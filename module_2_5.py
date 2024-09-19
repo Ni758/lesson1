702b992b57ab4b255d38118a2dd95431ce3347b0
Ni758 <Nina7812@gmail.com>
2024-09-19 22:17:12 +0500

    Update module_2_5.py
    почистила от принтов

--- module_2_5.py
+++ module_2_5.py
@@ -2,13 +2,11 @@ def get_matrix(n,m,value):
     matrix = []
     for row_i in range(n):
         matrix.append([])
-        print(matrix)
         for _ in range(m):
             matrix[row_i].append(value)
-            print(matrix)
     return matrix
 
 
-get_matrix(2, 2, 10)
-get_matrix(3, 5, 42)
-get_matrix(4, 2, 13)
+print(get_matrix(2, 2, 10))
+print(get_matrix(3, 5, 42))
+print(get_matrix(4, 2, 13))
