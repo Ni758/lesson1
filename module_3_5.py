0511ecf421899fed11725780448146a2e1acb19f
Ni758 <Nina7812@gmail.com>
2024-09-22 18:20:27 +0500

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif len(str_number) <= 1:
+        return first
+
+print(get_multiplied_digits(4023))
+print(get_multiplied_digits(203))
+print(get_multiplied_digits(3))
+
